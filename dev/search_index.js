8dd2c3042af379690e7d516f2a5223977af01281
Documenter.jl <documenter@juliadocs.github.io>
2024-06-18 14:15:22 +0000

    build based on 1592d0b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IntervalSySCoRe","category":"page"},{"location":"#IntervalSySCoRe","page":"Home","title":"IntervalSySCoRe","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IntervalSySCoRe.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IntervalSySCoRe]","category":"page"}]
+}
