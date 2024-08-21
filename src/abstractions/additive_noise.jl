940147ab41574d37990c8ec91fede6aca24ae069
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-08-21 18:44:52 +0200

    Prepare for decoupled IMDP abstraction

--- src/abstractions/additive_noise.jl
+++ src/abstractions/additive_noise.jl
@@ -5,7 +5,7 @@ export abstraction
 """
     abstraction(sys::System{<:AffineAdditiveNoiseDynamics}, state_abstraction::StateGridSplit, input_abstraction::InputAbstraction, target_model::Union{DirectIMDP, SparseDirectIMDP})
 
-Abstract function for creating an abstraction of a system with additive noise.
+Abstract function for creating an abstraction of a system with additive noise with an IMDP as the target model.
 """
 function abstraction(sys::System{<:AffineAdditiveNoiseDynamics}, state_abstraction::StateGridSplit, input_abstraction::InputAbstraction, target_model::Union{DirectIMDP, SparseDirectIMDP})
     # The first state is absorbing, representing transitioning to outside the partitioned.
@@ -86,4 +86,79 @@ function direct_initprob(target::SparseDirectIMDP, nregions, ninputs)
     prob_upper = copy(prob_lower)
 
     return prob_lower, prob_upper
+end
+
+"""
+    abstraction(sys::System{<:AffineAdditiveNoiseDynamics}, state_abstraction::StateGridSplit, input_abstraction::InputAbstraction, target_model::DecoupledIMDP)
+
+Abstract function for creating an abstraction of a system with additive noise with a decoupled IMDP as the target model.
+"""
+function abstraction(sys::System{<:AffineAdditiveNoiseDynamics}, state_abstraction::StateGridSplit, input_abstraction::InputAbstraction, target_model::DecoupledIMDP)
+    # The first state is absorbing, representing transitioning to outside the partitioned.
+    nregions = numregions(state_abstraction) + 1
+    ninputs = numinputs(input_abstraction)
+
+    prob_lower, prob_upper = direct_initprob(target_model, nregions, ninputs)
+
+    # Absorbing
+    prob_lower[1, 1] = 1.0
+    prob_upper[1, 1] = 1.0
+
+    # Transition probabilities
+    dyn = dynamics(sys)
+
+    # TODO: Ensure enumeration order is correct!! 
+    for (i, source_region) in enumerate(regions(state_abstraction))
+        for (j, input) in enumerate(inputs(input_abstraction))
+            Y = nominal(dyn, source_region, input)
+            srcact_idx = (i - 1) * ninputs + j + 1
+
+            # For each axis... 
+            
+            # Transition to outside the partitioned region
+            pl, pu = transition_prob_bounds(Y, statespace(state_abstraction), noise(dyn))
+            prob_lower[1, srcact_idx] = 1.0 - pu
+            prob_upper[1, srcact_idx] = 1.0 - pl
+
+            # Transition to other states
+            for (tar_idx, target_region) in enumerate(regions(state_abstraction))
+                pl, pu = transition_prob_bounds(Y, target_region, noise(dyn))
+
+                if includetransition(target_model, pu)
+                    prob_lower[tar_idx + 1, srcact_idx] = pl
+                    prob_upper[tar_idx + 1, srcact_idx] = pu
+                else  # Allow sparsifying via adding probability to the absorbing avoid state
+                    prob_lower[1, srcact_idx] += pl
+                    prob_upper[1, srcact_idx] += pu
+                end
+            end
+        end
+    end
+
+    prob = IntervalProbabilities(;lower=prob_lower, upper=prob_upper)
+
+    # State pointer
+    stateptr = Int32[[1, 2]; (1:nregions-1) .* ninputs .+ 2]
+
+    # Properties
+    initial_states = Int32[]
+    reach_states = Int32[]
+    avoid_states = Int32[1] # Absorbing state
+
+    for (i, source_region) in enumerate(regions(state_abstraction))
+        if !isdisjoint(initial(sys), source_region)
+            push!(initial_states, i + 1)
+        end
+
+        if reach(sys) ⊆ source_region
+            push!(reach_states, i + 1)
+        end
+
+        if !isdisjoint(avoid(sys), source_region)
+            push!(avoid_states, i + 1)
+        end
+    end
+
+    # Final construction
+    return IntervalMarkovDecisionProcess(prob, stateptr, initial_states), reach_states, avoid_states
 end
---
+++ test/abstractions/decoupled.jl
@@ -0,0 +1,76 @@
+using Revise, Test
+using LinearAlgebra, LazySets
+using IntervalMDP, IntervalSySCoRe
+
+include("example_systems.jl")
+
+function simple_1d_decoupled()
+    sys = simple_1d_sys()
+
+    X = Hyperrectangle(; low=[-2.5], high=[2.5])
+    state_abs = StateGridSplit(X, [10])
+    input_abs = InputDiscrete([Singleton([0.0])])
+    target_model = DecoupledIMDP()
+
+    mdp, reach, avoid = abstraction(sys, state_abs, input_abs, target_model)
+
+    return mdp, reach, avoid
+end
+
+mdp_decoupled, reach_decoupled, avoid_decoupled = simple_1d_decoupled()
+@test num_states(mdp_decoupled) == 11
+@test stateptr(mdp_decoupled)[end] == 12
+
+prop_decoupled = FiniteTimeReachAvoid(reach_decoupled, avoid_decoupled, 10)
+spec_decoupled = Specification(prop_decoupled, Pessimistic, Maximize)
+prob_decoupled = Problem(mdp_decoupled, spec_decoupled)
+
+V_dense_grid, k, res = value_iteration(prob_decoupled)
+@test k == 10
+
+function running_example_decoupled(; range_vs_grid=:grid)
+    sys = running_example_sys()
+
+    X = Hyperrectangle(; low=[-10.0, -10.0], high=[10.0, 10.0])
+    state_abs = StateGridSplit(X, [20, 20])
+
+    U = Hyperrectangle(; low=[-1.0, -1.0], high=[1.0, 1.0])
+    if range_vs_grid == :range
+        input_abs = InputLinRange(U, [3, 3])
+    elseif range_vs_grid == :grid
+        input_abs = InputGridSplit(U, [3, 3])
+    else
+        throw(ArgumentError("Invalid range_vs_grid argument"))
+    end
+
+    target_model = DecoupledIMDP()
+
+    mdp, reach, avoid = abstraction(sys, state_abs, input_abs, target_model)
+
+    return mdp, reach, avoid
+end
+
+# Input grid
+mdp_decoupled, reach_decoupled, avoid_decoupled = running_example_decoupled()
+@test num_states(mdp_decoupled) == 401
+@test stateptr(mdp_decoupled)[end] == 20 * 20 * 9 + 2
+
+prop_decoupled = FiniteTimeReachAvoid(reach_decoupled, avoid_decoupled, 10)
+spec_decoupled = Specification(prop_decoupled, Pessimistic, Maximize)
+prob_decoupled = Problem(mdp_decoupled, spec_decoupled)
+
+V_grid, k, res = value_iteration(prob_decoupled)
+@test k == 10
+
+# Input range
+mdp_decoupled, reach_decoupled, avoid_decoupled = running_example_decoupled(; range_vs_grid=:range)
+@test num_states(mdp_decoupled) == 401
+@test stateptr(mdp_decoupled)[end] == 20 * 20 * 9 + 2
+
+prop_decoupled = FiniteTimeReachAvoid(reach_decoupled, avoid_decoupled, 10)
+spec_decoupled = Specification(prop_decoupled, Pessimistic, Maximize)
+prob_decoupled = Problem(mdp_decoupled, spec_decoupled)
+
+V_range, k, res = value_iteration(prob_decoupled)
+@test k == 10
+@test all(V_range .≥ V_grid)
