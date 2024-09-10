1700761b4595247ecddb82fbeed7cf2e264b7ba0
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-09-10 10:59:40 +0200

    Add 4D and 7D building automation system

---
+++ examples/systems/bas_4d.jl
@@ -0,0 +1,61 @@
+using LinearAlgebra, LazySets
+using IntervalMDP, IntervalSySCoRe
+
+
+function building_automation_system_4d()
+    A = [
+        0.6682 0.0    0.02632   0.0;
+        0.0    0.683  0.0       0.02096;
+        1.0005 0.0    -0.000499 0.0;
+        0.0    0.8004 0.0       0.1996
+    ]
+
+    B = [
+        0.1320;
+        0.1402;
+        0.0;
+        0.0
+    ]
+    
+    C = [3.4378, 2.9272, 13.0207, 10.4166]
+
+    w_variance = [1/12.9199, 1/12.9199, 1/2.5826, 1/3.2276]
+    w_stddev = sqrt.(w_variance)
+
+    dyn = AffineAdditiveNoiseDynamics(A, B, C, AdditiveDiagonalGaussianNoise(w_stddev))
+
+    initial_region = EmptySet(4)
+    reach_region = EmptySet(4)
+    avoid_region = EmptySet(4)
+
+    sys = System(dyn, initial_region, reach_region, avoid_region)
+
+    return sys
+end
+
+function building_automation_system_4d_decoupled(; state_split=(4, 4, 6, 6), input_split=4)
+    sys = c()
+
+    X = Hyperrectangle(; low=[19.0, 19.0, 30.0, 30.0], high=[21.0, 21.0, 36.0, 36.0])
+    state_abs = StateUniformGridSplit(X, state_split)
+
+    U = Hyperrectangle(; low=[17.0], high=[20.0])
+    input_abs = InputLinRange(U, input_split)
+
+    target_model = DecoupledIMDP()
+
+    mdp, reach, avoid = abstraction(sys, state_abs, input_abs, target_model)
+
+    return mdp, reach, avoid
+end
+
+function main()
+    @time "abstraction" mdp, _, avoid = building_automation_system_4d_decoupled(; state_split=(4, 4, 6, 6), input_split=4)
+
+    prop = FiniteTimeReachability(avoid, 10)
+    spec = Specification(prop, Optimistic, Minimize)
+    prob = Problem(mdp, spec)
+
+    @time "value iteration" V_unsafety, k, res = value_iteration(prob)
+    V_safety = 1.0 .- V_unsafety
+end
---
+++ examples/systems/bas_7d.jl
@@ -0,0 +1,56 @@
+using LinearAlgebra, LazySets
+using IntervalMDP, IntervalSySCoRe
+
+
+function building_automation_system_7d()
+    A = [
+        0.9678 0.0    0.0036 0.0    0.0036 0.0    0.0036;
+        0.0    0.9682 0.0    0.0034 0.0    0.0034 0.0034;
+        0.0106 0.0    0.9494 0.0    0.0    0.0    0.0;
+        0.0    0.0097 0.0    0.9523 0.0    0.0    0.0;
+        0.0106 0.0    0.0    0.0    0.9494 0.0    0.0;
+        0.0    0.0097 0.0    0.0    0.0    0.9523 0.0;
+        0.0106 0.0097 0.0    0.0    0.0    0.0    0.9794;
+    ]
+
+    B = zero(Float64, 7, 1)
+
+    w_variance = [1/51.2821, 1/50.0, 1/21.7865, 1/23.5294, 1/25.1889, 1/26.5252, 1/91.7431]
+    w_stddev = sqrt.(w_variance)
+
+    dyn = AffineAdditiveNoiseDynamics(A, B, AdditiveDiagonalGaussianNoise(w_stddev))
+
+    initial_region = EmptySet(7)
+    reach_region = EmptySet(7)
+    avoid_region = EmptySet(7)
+
+    sys = System(dyn, initial_region, reach_region, avoid_region)
+
+    return sys
+end
+
+function building_automation_system_7d_decoupled(; state_split=(20, 20, 2, 2, 2, 2, 2))
+    sys = c()
+
+    X = Hyperrectangle(; low=fill(-0.5, 7), high=fill(0.5, 7))
+    state_abs = StateUniformGridSplit(X, state_split)
+
+    input_abs = InputDiscrete([Singleton(zero(7))])
+
+    target_model = DecoupledIMDP()
+
+    mdp, reach, avoid = abstraction(sys, state_abs, input_abs, target_model)
+
+    return mdp, reach, avoid
+end
+
+function main()
+    @time "abstraction" mdp, _, avoid = building_automation_system_7d_decoupled(; state_split=(20, 20, 2, 2, 2, 2, 2))
+
+    prop = FiniteTimeReachability(avoid, 10)
+    spec = Specification(prop, Optimistic, Minimize)
+    prob = Problem(mdp, spec)
+
+    @time "value iteration" V_unsafety, k, res = value_iteration(prob)
+    V_safety = 1.0 .- V_unsafety
+end
