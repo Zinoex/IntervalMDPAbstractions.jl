e9e459bc4c41e189eb2af9bf2f07ab0d9946bfc0
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-09-24 10:24:34 +0200

    Add iszeromeasure methods for EmpySet

--- src/utils.jl
+++ src/utils.jl
@@ -35,3 +35,7 @@ end
 function iszeromeasure(X::AbstractHyperrectangle, Y::AbstractHyperrectangle)
     return any(high(Y, i) ≤ low(X, i) || high(X, i) ≤ low(Y, i) for i in 1:LazySets.dim(X))
 end
+
+iszeromeasure(X::EmptySet, Y::LazySet) = true
+iszeromeasure(X::LazySet, Y::EmptySet) = true
+iszeromeasure(X::EmptySet, Y::EmptySet) = true
