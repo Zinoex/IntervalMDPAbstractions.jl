d6b0707e50c7db8cb22d52a5d57e093531a4c5f2
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-09-30 15:53:25 +0200

    Add almost_identity.jl to systems.jl

--- examples/systems/systems.jl
+++ examples/systems/systems.jl
@@ -2,6 +2,7 @@ include("running_example.jl")
 include("robot_2d.jl")
 include("bas_4d.jl")
 include("bas_7d.jl")
+include("almost_identity.jl")
 
 include("integrator_chain.jl")
 include("van_der_pol.jl")
