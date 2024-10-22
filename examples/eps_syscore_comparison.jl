d2c6173b2a1e3db3c78600f89685b28bb6324ac8
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-10-22 14:20:50 +0200

    Change measure for comparison with SySCoRe

--- examples/eps_syscore_comparison.jl
+++ examples/eps_syscore_comparison.jl
@@ -1,8 +1,8 @@
 module EpsSySCoReComparison
 
 using BenchmarkTools
-using IntervalMDP
-using IntervalSySCoRe
+using IntervalMDP, IntervalSySCoRe
+using Statistics
 
 using MAT, MAT.MAT_v4, MAT.MAT_v5, MAT.MAT_HDF5
 
@@ -19,12 +19,12 @@ struct EpsSySCoReComparisonProblem
 end
 
 
-syscore_running_example = EpsSySCoReComparisonProblem(
-    "running_example",
-    (state_split, input_split, time_horizon) -> running_example_decoupled(time_horizon; range_vs_grid=:range, state_split=state_split, input_split=input_split),
+car_parking = EpsSySCoReComparisonProblem(
+    "car_parking",
+    (state_split, input_split, time_horizon) -> car_parking_decoupled(time_horizon; range_vs_grid=:range, state_split=state_split, input_split=input_split),
     [(50, 50), (100, 100), (150, 150)],
     (3, 3),
-    [1, 2, 5, 10, 20, 50, 100, 200]
+    [[1, 2]; collect(5:5:100)]
 )
 
 
@@ -33,34 +33,33 @@ bas_4d = EpsSySCoReComparisonProblem(
     (state_split, input_split, time_horizon) -> building_automation_system_4d_decoupled(time_horizon; state_split=state_split, input_split=input_split),
     [(4, 4, 4, 4), (8, 8, 8, 8), (12, 12, 12, 12), (16, 16, 16, 16)],
     4,
-    [1, 2, 5, 10, 20, 50, 100, 200]
+    [[1, 2]; collect(5:5:100)]
 )
 
 function benchmark_intervalsyscore(problem::EpsSySCoReComparisonProblem, state_split, time_horizon)
     BenchmarkTools.gcscrub()
 
-    mdp, lower_spec = problem.constructor(state_split, problem.input_split, time_horizon)
-    lower_bound_prob = Problem(mdp, lower_spec)
+    mdp, lower_spec, upper_spec = problem.constructor(state_split, problem.input_split, time_horizon)
 
+    lower_bound_prob = Problem(mdp, lower_spec)
     strategy, V_lower, k, res = control_synthesis(lower_bound_prob)
 
-    upper_spec = Specification(system_property(lower_spec), !satisfaction_mode(lower_spec))
     upper_bound_prob = Problem(mdp, upper_spec, strategy)
     V_upper, k, res = value_iteration(upper_bound_prob)
 
     V_eps = V_upper - V_lower
     V_eps[terminal_states(system_property(lower_spec))] .= 0.0
 
-    max_V_eps = maximum(V_eps)
+    mean_V_eps = mean(V_eps)
 
-    return max_V_eps
+    return mean_V_eps
 end
 
 function benchmark_system(problem, state_split, time_horizon)
     println(("num_regions_per_axis", state_split, "time_horizon", time_horizon))
-    max_V_eps = benchmark_intervalsyscore(problem, state_split, time_horizon)
+    mean_V_eps = benchmark_intervalsyscore(problem, state_split, time_horizon)
 
-    println(("max_V_eps", max_V_eps))
+    println(("mean_V_eps", mean_V_eps))
 end
 
 function benchmark_system(problem::EpsSySCoReComparisonProblem, time_horizon)
