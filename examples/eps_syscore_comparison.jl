592a324915232529d0711192c8bf328a1daf2372
Frederik Baymler Mathiesen <frederik@baymler.com>
2024-10-25 18:07:49 +0200

    Use the callback to make eps_syscore_comparison faster

--- examples/eps_syscore_comparison.jl
+++ examples/eps_syscore_comparison.jl
@@ -43,27 +43,60 @@ function retime_spec(prob, spec, time_horizon)
     return Specification(prop, satisfaction_mode(spec), strategy_mode(spec))
 end
 
-function benchmark_intervalsyscore(mdp, lower_spec, upper_spec)
+function benchmark_intervalsyscore(mdp, lower_spec, upper_spec, time_horizons)
     BenchmarkTools.gcscrub()
 
+    @assert time_horizon(system_property(lower_spec)) == time_horizon(system_property(upper_spec))
+    @assert time_horizon(system_property(lower_spec)) <= maximum(time_horizons)
+
+    V_lowers = Vector{AbstractArray{Float64}}(undef, length(time_horizons))
+    V_uppers = Vector{AbstractArray{Float64}}(undef, length(time_horizons))
+
+    function lower_callback(V, k)
+        for (i, time_horizon) in enumerate(time_horizons)
+            if k == time_horizon
+                V_lowers[i] = copy(V)
+            end
+        end
+    end
+
     lower_bound_prob = Problem(mdp, lower_spec)
-    strategy, V_lower, k, res = control_synthesis(lower_bound_prob)
+    strategy, _, k, res = control_synthesis(lower_bound_prob; callback=lower_callback)
+
+    function upper_callback(V, k)
+        for (i, time_horizon) in enumerate(time_horizons)
+            if k == time_horizon
+                V_uppers[i] = copy(V)
+            end
+        end
+    end
 
     upper_bound_prob = Problem(mdp, upper_spec, strategy)
-    V_upper, k, res = value_iteration(upper_bound_prob)
+    _, k, res = value_iteration(upper_bound_prob; callback=upper_callback)
 
-    V_eps = V_upper - V_lower
-    # This is a bit of a hack, but it is a way to prune the terminal states,
-    # aka. keep non-terminal states only (which is the hard part).
-    # We only keep non-terminal states since only they are relevant for the
-    # benchmark. 
-    V_eps[terminal_states(system_property(lower_spec))] .= -1.0
-    V_eps = V_eps[V_eps .>= 0.0]
+    tstates = terminal_states(system_property(lower_spec))
 
-    mean_V_eps = mean(V_eps)
-    max_V_eps = maximum(V_eps)
+    V_epses = Vector{Vector{Float64}}(undef, length(time_horizons))
 
-    return mean_V_eps, max_V_eps
+    for i in eachindex(time_horizons)
+        V_lower = V_lowers[i]
+        V_upper = V_uppers[i]
+
+        V_eps = V_upper - V_lower
+        # This is a bit of a hack, but it is a way to prune the terminal states,
+        # aka. keep non-terminal states only (which is the hard part).
+        # We only keep non-terminal states since only they are relevant for the
+        # benchmark. 
+        V_eps[tstates] .= -1.0
+        V_eps = V_eps[V_eps .>= 0.0]
+
+        V_epses[i] = V_eps
+    end
+
+    means = mean.(V_epses)
+    maxs = maximum.(V_epses)
+
+    return means, maxs
 end
 
 function construct_model(problem, state_split, time_horizon=1)
@@ -77,21 +110,22 @@ function benchmark_system(problem, state_split, time_horizon)
     println(("num_regions_per_axis", state_split, "time_horizon", time_horizon))
     mdp, lower_spec, upper_spec = construct_model(problem, state_split, time_horizon)
 
-    mean_V_eps, max_V_eps = benchmark_intervalsyscore(mdp, lower_spec, upper_spec)
+    mean_V_eps, max_V_eps = benchmark_intervalsyscore(mdp, lower_spec, upper_spec, [time_horizon])
 
-    println(("mean_V_eps", mean_V_eps, "max_V_eps", max_V_eps))
+    println(("mean_V_eps", mean_V_eps[1], "max_V_eps", max_V_eps[1]))
 end
 
 function benchmark_system(problem::EpsSySCoReComparisonProblem, state_split)
     mdp, lower_spec, upper_spec = construct_model(problem, state_split)
+    lower_spec = retime_spec(problem, lower_spec, maximum(problem.time_horizons))
+    upper_spec = retime_spec(problem, upper_spec, maximum(problem.time_horizons))
 
-    for time_horizon in problem.time_horizons
-        println(("num_regions_per_axis", state_split, "time_horizon", time_horizon))
-        lower_spec = retime_spec(problem, lower_spec, time_horizon)
-        upper_spec = retime_spec(problem, upper_spec, time_horizon)
+    println(("num_regions_per_axis", state_split))
 
-        mean_V_eps, max_V_eps = benchmark_intervalsyscore(mdp, lower_spec, upper_spec)
-        println(("mean_V_eps", mean_V_eps, "max_V_eps", max_V_eps))
+    mean_V_eps, max_V_eps = benchmark_intervalsyscore(mdp, lower_spec, upper_spec, problem.time_horizons)
+    
+    for (time_horizon, mean_V_ep, max_V_ep) in zip(problem.time_horizons, mean_V_eps, max_V_eps)
+        println(("time_horizon", time_horizon, "mean_V_eps", mean_V_ep, "max_V_eps", max_V_ep))
     end
 end
 
