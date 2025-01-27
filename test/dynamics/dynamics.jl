91793d1b16829bfe2d92e9eeed35f89a4df5ac67
Frederik Baymler Mathiesen <frederik@baymler.com>
2025-01-27 13:32:30 +0100

    Prepare testing gaussian processes and stochastic switched dynamics

--- test/dynamics/dynamics.jl
+++ test/dynamics/dynamics.jl
@@ -1,5 +1,5 @@
 
-test_files = ["affine_additive_gaussian.jl"]
+test_files = ["additive_noise.jl", "affine_additive.jl", "nonlinear.jl", "uncertain_pwa.jl", "gaussian_process.jl", "stochastical_switched.jl"]
 for f in test_files
     @testset "dynamics/$f" include(f)
 end
