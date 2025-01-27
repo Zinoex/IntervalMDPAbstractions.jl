f857844c1257766e172ae6611ec0d234a50de60d
Frederik Baymler Mathiesen <frederik@baymler.com>
2025-01-27 13:31:39 +0100

    Test affine dynamics

--- test/dynamics/affine_additive.jl
+++ test/dynamics/affine_additive.jl
@@ -6,8 +6,15 @@ A = [1.0 0.5; 0.0 1.0]
 B = reshape([0.0; 1.0], (2, 1))
 
 w_stddev = [0.1, 0.1]
+w = AdditiveDiagonalGaussianNoise(w_stddev)
 
-dyn = AffineAdditiveNoiseDynamics(A, B, AdditiveDiagonalGaussianNoise(w_stddev))
+dyn = AffineAdditiveNoiseDynamics(A, B, w)
+initial_region = Hyperrectangle(low = [-1.0, -1.0], high = [1.0, 1.0])
+sys = System(dyn, initial_region)
+
+@test noise(dyn) == w
+@test dimstate(sys) == 2
+@test diminput(sys) == 1
 
 # Hyperrectangular regions
 X = Hyperrectangle(low = [0.0, 0.0], high = [1.0, 1.0])
