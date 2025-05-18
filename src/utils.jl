cb7b0117fcb332c7c2cea2635779f9b07e51b618
Frederik Baymler Mathiesen <frederik@baymler.com>
2025-05-18 17:40:27 +0200

    Add efficient iszeromeasure for CartesianProduct with Universe

--- src/utils.jl
+++ src/utils.jl
@@ -10,6 +10,30 @@ iszeromeasure(X::EmptySet, Y::EmptySet) = true
 
 iszeromeasure(X::AbstractPolyhedron, Y::AbstractPolyhedron) = _iszeromeasure(Y, X)
 
+function iszeromeasure(C::CartesianProduct{N,<:LazySet,<:Universe}, Z::AbstractHyperrectangle) where {N}
+    X = first(C)
+    Zp = project(Z, 1:dim(X))
+    return iszeromeasure(X, Zp)
+end
+
+function iszeromeasure(Z::AbstractHyperrectangle, C::CartesianProduct{N,<:LazySet,<:Universe}) where {N}
+    X = first(C)
+    Zp = project(Z, 1:dim(X))
+    return iszeromeasure(X, Zp)
+end
+
+function iszeromeasure(C::CartesianProduct{N,<:Universe,<:LazySet}, Z::AbstractHyperrectangle) where {N}
+    Y = second(C)
+    Zp = project(Z, (dim(first(C)) + 1):dim(C))
+    return iszeromeasure(Y, Zp)
+end
+
+function iszeromeasure(Z::AbstractHyperrectangle, C::CartesianProduct{N,<:Universe,<:LazySet}) where {N}
+    Y = second(C)
+    Zp = project(Z, (dim(first(C)) + 1):dim(C))
+    return iszeromeasure(Y, Zp)
+end
+
 function iszeromeasure(X::LazySet, Y::LazySet)
     if ispolyhedral(X) && ispolyhedral(Y)
         return _iszeromeasure(X, Y)
